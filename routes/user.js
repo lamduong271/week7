4b384229edab3d229fa0c25d00d28e850128a1fa
lamduong96 <lam.duong@smartly.io>
2022-06-30 15:06:18 +0300

    token

--- routes/user.js
+++ routes/user.js
@@ -2,24 +2,30 @@ var express = require("express");
 var router = express.Router();
 var User = require("../models/user.model");
 var bcrypt = require("bcrypt");
+var jsonWebToken = require("jsonwebtoken");
 router.get("/user/register", (req, res) => {
   res.send("Welcome to authentication tutorial");
 });
 
+var JSON_SECRET_CODE = "thiIsADog";
+
+const saltRounds = 10;
+
 router.post("/user/register", async (req, res) => {
   console.log(req.body);
-  const saltRounds = 10;
   try {
     const existUser = await User.findOne({ email: req.body.email });
+    console.log("existUser ", existUser);
     if (existUser) {
       res.send("Email already in use.");
+    } else {
+      const hashedPwd = await bcrypt.hash(req.body.password, saltRounds);
+      await User.create({
+        email: req.body.email,
+        password: hashedPwd,
+      });
+      res.send("ok");
     }
-    const hashedPwd = await bcrypt.hash(req.body.password, saltRounds);
-    await User.create({
-      username: req.body.username,
-      password: hashedPwd,
-    });
-    res.send("ok");
   } catch (error) {
     console.log(error);
     res.status(500).send("Internal Server error Occured");
@@ -29,12 +35,24 @@ router.post("/user/register", async (req, res) => {
 router.post("/user/login", async (req, res) => {
   try {
     const existUser = await User.findOne({ email: req.body.email });
+
     if (existUser) {
-      const comparedPwd = await bcrypt.compare(req.body.password, saltRounds);
+      const comparedPwd = await bcrypt.compare(
+        req.body.password,
+        existUser.password
+      );
       if (comparedPwd) {
-        res.send({ success: true });
+        var token = jsonWebToken.sign(
+          {
+            id: existUser._id,
+            email: existUser.email,
+          },
+          JSON_SECRET_CODE
+        );
+        res.send({ success: true, token });
+      } else {
+        res.status(401).send({ message: "not found" });
       }
-      res.status(401).send({ message: "not found" });
     }
   } catch (error) {
     console.log(error);
